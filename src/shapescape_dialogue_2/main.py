2de924286b2a9f5d437a985d14decb85f4c61073
Nusiq <34062025+Nusiq@users.noreply.github.com>
2022-05-31 13:23:31 +0200

    Basic implementation of the parser

--- src/shapescape_dialogue_2/main.py
+++ src/shapescape_dialogue_2/main.py
@@ -1,10 +1,20 @@
-from .parser import tokenize
+from .parser import ParseError, tokenize, build_ast
 from pathlib import Path
 
 def main():
     source_file = Path("test.txt")
+    output_file = Path("log.txt")
+
     with source_file.open("r", encoding='utf8') as f:
         source = f.readlines()
     tokens = tokenize(source)
-    for token in tokens:
-        print(token)
+    with output_file.open("w", encoding='utf8') as f:
+        for token in tokens:
+            print(token, file=f)
+        tree = build_ast(tokens)
+        print(tree, file=f)
+        # try:
+        #     tree = build_ast(tokens)
+        #     print(tree, file=f)
+        # except ParseError as e:
+        #     print(str(e))
